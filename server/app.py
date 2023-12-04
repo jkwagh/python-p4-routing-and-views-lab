1c0f7950cbfca538214c9a6240fe98512dbf33ab
Jeremy <kwagh.jeremiah@gmail.com>
2023-12-04 14:23:11 -0500

    submit lab assignment

--- server/app.py
+++ server/app.py
@@ -6,3 +6,34 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<parameter>')
+def print_string(parameter):
+    print(parameter)
+    return f'{parameter}'
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    i = ""
+    for num in range(parameter):
+        i += f'{num}\n'
+    return i
+
+@app.route('/math/<int:num1><operation><int:num2>')
+def math(num1, num2, operation):
+    result = ""
+    if operation == "+":
+        result = num1 + num2
+    elif operation == "-":
+        result = num1 - num2
+    elif operation == "*":
+        result = num1 * num2
+    elif operation == "div":
+        result = num1 / num2
+    elif operation == "%":
+        result = num1 % num2
+    return f'{result}'  
